0de71dcfa3de6af183410b0c5ff9d5d6b9074b52
AlexandrTurov <alexandr.turov@mail.ru>
2025-04-11 00:19:10 +0700

    feat(devtolls): add devtools

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import { useTimeService } from './services/time'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
 const { now } = useTimeService(500);
 export const useNow = () => now;
 
-const app = createApp(App)
+const app = createApp(App);
 
 app.config.errorHandler = (...args) => {
   console.error(...args)
 }
+
+app.use(vueDevTools);
 
 app.mount('#app')
